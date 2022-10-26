2a5758b81f04832e67f5d253b45f54a65a17d5ab
facu <facu.palavecino0727@gmail.com>
2022-10-26 16:27:02 -0300

    segundo commit

--- src/Album.java
+++ src/Album.java
@@ -1,7 +1,8 @@
+import java.util.HashMap;
 
 public abstract class Album {
 	private String codigoID;
 	private String premio;
 	private boolean estaCompleto;
-	private HashMap <String>a;
+	//private HashMap <String, int v> a;
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,4 +1,7 @@
 
 public class Usuario {
-
+	private String nombreUsuario;
+	private int dni;
+	private Album album;
+	//private Conjunto<Figurita> figRepetidas; 
 }
