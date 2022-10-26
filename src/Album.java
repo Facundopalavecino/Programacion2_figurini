bafbcfee7b76f65817893b8cfcd044711398b163
Facu Palavecino <facu.palavecino0727@gmail.com>
2022-10-26 16:04:58 -0300

    AAa

--- src/Album.java
+++ src/Album.java
@@ -1,4 +1,7 @@
 
 public abstract class Album {
-
+	private String codigoID;
+	private String premio;
+	private boolean estaCompleto;
+	private HashMap <String>a;
 }
--- src/Figurita.java
+++ src/Figurita.java
@@ -1,4 +1,8 @@
 
 public abstract class Figurita {
-
+	private int numero;
+	private int valorBase;
+	private String nombrePais;
+	private String nombreJugador;
+	
 }
